3ea63ce3fab6f0e669229c0450ab2bf5fefbe9d8
mieczkowski4327 <nickmieczkowskiski@gmail.com>
2020-03-13 13:41:45 -0500

    comment

--- src/com/company/gameObjects/Projectile.java
+++ src/com/company/gameObjects/Projectile.java
@@ -11,7 +11,8 @@ public class Projectile extends GameObject {
     private boolean fire;
     private int speed;
     private double x, y;
-
+    private double a;
+    private Point p = new Point();
     public Projectile(int x, int y, double angle, BufferedImage img, boolean fire, int speed) {
         super(0,0);
         this.x = x;
@@ -27,11 +28,15 @@ public class Projectile extends GameObject {
         xVel = (float) (Math.cos(angle) * speed);
         yVel = (float) (Math.sin(angle) * speed);
         if (fire) {
-            Main.room.addParticle((int)x + getBounds().width / 2, (int)y + getBounds().height / 2, 1, 1, 1, 1, .3f, .5f, 1f, false, false, false, 0, 0, 40, 80, Color.orange, Color.yellow, 5f);
-            Main.room.addParticle((int)x + getBounds().width / 2, (int)y + getBounds().height / 2, 1, 1, 1, 1, .3f, .5f, 4, false, false, false, 0, 0, 6, 10, Color.red, Color.orange, .1f);
+            int rectSize = Loader.randomInt(40,80);
+            int rectSize2 = Loader.randomInt(6,10);
+            p = Loader.rotatePoint((int)x + getBounds().width / 2, (int)y + getBounds().height / 2, a, new Point((int)x + getBounds().width / 2 - rectSize / 2, (int)y + getBounds().height / 2 - rectSize / 2));
+            Main.room.addParticle(p.x, p.y, 1, 1, 1, 1, .3f, .5f, 1f, false, false, false, 0, 0, rectSize, Color.orange, Color.yellow, 5f);
+            Main.room.addParticle((int)x + getBounds().width / 2, (int)y + getBounds().height / 2, 1, 1, 1, 1, .3f, .5f, 4, false, false, false, 0, 0, rectSize2, Color.red, Color.orange, .1f);
         }
-        x += xVel;
-        y += yVel;
+        //x += xVel;
+        //y += yVel;
+        a += .1;
     }
 
     public int getX() {
@@ -44,6 +49,8 @@ public class Projectile extends GameObject {
 
     public void render(Graphics2D g) {
         Loader.renderRotatedImage(g, g.getTransform(), angle, (int)x + getBounds().width / 2, (int)y + getBounds().height / 2, img, (int)x, (int)y);
+        g.setColor(Color.blue);
+        g.fillRect(p.x, p.y, 3, 3);
     }
 
     public Rectangle getBounds() {
